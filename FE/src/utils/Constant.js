70c00926aa3308448564e94c441ca57785a2c830
A. Developers <hinaarooj978@gmail.com>
2025-01-29 19:50:47 +0500

    fix typo in comment for local server in Constant.js

--- FE/src/utils/Constant.js
+++ FE/src/utils/Constant.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Local server
   // baseUrl: "http://localhost:4000/api/v1",
-  // Local server
+  // Locala server
   // Official server 
   baseUrl: "https://bitblaze.onrender.com/api/v1",
   // Official server
